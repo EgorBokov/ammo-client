b2a7b4bbb203b9f8d906aa8e44faff2000539318
Egor Bokov <s1lice.egor@gmail.com>
2022-12-13 19:42:25 +0300

    Исрпалвен внейшний вид главной страницы

--- components/TheContainer.vue
+++ components/TheContainer.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex flex-col gap-[8px] items-center px-[25px] py-[10px]">
+    <div class="flex flex-col gap-[8px] items-center px-[10px] min-[620px]:px-[25px] py-[10px]">
       <div v-if="title" class="p-[10px] w-full bg-[#fff] rounded-[15px] max-w-[1000px] min-h-[50px]">
         <TheTitle :content="title" />
       </div>
-      <div 
+      <div
         class="p-[10px] w-full rounded-[15px] max-w-[1000px] min-h-[50px]"
         :class="[isTransparentContent ? '' : 'bg-[#fff]']"
       >
--- pages/index.vue
+++ pages/index.vue
@@ -4,23 +4,23 @@
       isTransparentContent
   >
     <div v-if="pending">Loading...</div>
-    <div v-else class="p-[10px] flex flex-col gap-3">
-      <div class="flex gap-3">
-        <div class="min-w-[180px] max-w-[180px] bg-[#fff] p-[10px] rounded-[20px]">
+    <div v-else class="p-[2px] min-[620px]:p-[10px] flex flex-col gap-3">
+      <div class="flex flex-col min-[665px]:flex-row gap-3">
+        <div class="min-w-[180px] hidden min-[665px]:block max-w-[180px] bg-[#fff] p-[10px] rounded-[20px]">
           <img src="/guarantee.svg" alt="Знак качества">
         </div>
         <div class="flex rounded-[20px] items-start p-[10px] bg-[#fff] w-full justify-end">
-          <div class="flex items-center gap-[10px]">
-            <h2 class="font-bold text-xl">Контактные данные:</h2>
+          <div class="flex items-start gap-[10px]">
+            <h2 class="font-bold text-xs min-[620px]:text-xl">Контактные данные:</h2>
             <div class="flex flex-col items-end">
-              <h2 class="font-bold text-xl">+7(995)685-96-60</h2>
-              <p>ammunition.rf@gmail.com</p>
+              <h2 class="font-bold self-start text-xs min-[620px]:text-xl">+7(995)685-96-60</h2>
+              <p class="text-xs min-[620px]:text-xl">ammunition.rf@gmail.com</p>
             </div>
           </div>
         </div>
       </div>
-      <div class="flex gap-3">
-        <div class="flex flex-col gap-[10px] min-w-[180px] max-w-[180px] flex-wrap min-w-[150px] bg-[#fff] rounded-[20px] p-[10px]">
+      <div class="flex flex-col gap-3">
+        <div class="flex flex-row items-center gap-[10px] w-full flex-wrap bg-[#fff] rounded-[20px] p-[10px]">
           <NuxtLink
               class="border-2 min-w-[92px] p-2 rounded-full border-[rgb(255,215,44)] cursor-pointer duration-300 hover:bg-bumblebee text-xs lg:text-base"
               v-for="link in data"
@@ -55,10 +55,30 @@
           </article>
         </div>
       </div>
+      <div>
+        <div class="p-[20px] w-full bg-[#fff] rounded-[15px] max-w-[1000px] min-h-[50px]">
+          <TheTitle content="Популярные позиции" />
+        </div>
+        <div
+            class="p-[20px] rounded-[20px] mt-[10px] grid grid-cols-1 bg-white min-[600px]:grid-cols-2 md:grid-cols-3 gap-[10px]"
+        >
+          <TheProductItem
+              v-for="item in popularPositions"
+              :key="item.id"
+              :id="item.productId"
+              :name="item.name"
+              :price="`${item.price}`"
+              :route="item.route"
+          >
+            <img class="max-h-[200px]" :src="`${BACKEND_URL}${item.image}`" alt="Изображение товара">
+          </TheProductItem>
+        </div>
+      </div>
     </div>
   </TheContainer>
 </template>
 <script setup lang="ts">
+import popularPositions from "~/utils/popularProducts";
 const config = useRuntimeConfig()
 const BACKEND_URL = config.public.backendURL
 
